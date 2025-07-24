66a035b206a134cb1452186db7b9805267b5f2e7
beaucoburn <douglas.coburn@gmail.com>
2025-07-24 11:29:30 -0600

    modified:   fashion-starter/medusa/src/api/store/custom/product-types/validators.ts

--- fashion-starter/medusa/src/api/store/custom/product-types/validators.ts
+++ fashion-starter/medusa/src/api/store/custom/product-types/validators.ts
@@ -1,30 +1,22 @@
-import {
-  createSelectParams,
-  createFindParams,
-  createOperatorMap,
-} from '@medusajs/medusa/api/utils/validators';
 import { z } from 'zod';
 
 export type AdminGetProductTypeParamsType = z.infer<
   typeof AdminGetProductTypeParams
 >;
-export const AdminGetProductTypeParams = createSelectParams();
+export const AdminGetProductTypeParams = z.object({
+  fields: z.string().optional(),
+});
 
 export type AdminGetProductTypesParamsType = z.infer<
   typeof AdminGetProductTypesParams
 >;
-export const AdminGetProductTypesParams = createFindParams({
-  limit: 10,
-  offset: 0,
-}).merge(
-  z.object({
-    q: z.string().optional(),
-    id: z.union([z.string(), z.array(z.string())]).optional(),
-    value: z.union([z.string(), z.array(z.string())]).optional(),
-    // TODO: To be added in next iteration
-    // discount_condition_id: z.string().nullish(),
-    created_at: createOperatorMap().optional() as any,
-    updated_at: createOperatorMap().optional() as any,
-    deleted_at: createOperatorMap().optional() as any,
-  }),
-);
+export const AdminGetProductTypesParams = z.object({
+  limit: z.number().default(10),
+  offset: z.number().default(0),
+  q: z.string().optional(),
+  id: z.union([z.string(), z.array(z.string())]).optional(),
+  value: z.union([z.string(), z.array(z.string())]).optional(),
+  created_at: z.string().optional(),
+  updated_at: z.string().optional(),
+  deleted_at: z.string().optional(),
+});
